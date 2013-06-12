c260c8ec4f3aa3382c7d4c57f14501ecbad92edb
Courtney Wallace <crwallace39@gmail.com>
2013-06-12 17:00:17 -0400

    Scripts with Some interface done

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,45 @@
-function hotCold(number) {
-    var number = null;
-    var gotcha = Math.floor((Math.random() * 10) + 1);
-    while (number!=gotcha){
-        number = prompt("Give me your best guess", "");
+$(document).ready(function () {
+    
+    
+    var gotcha = Math.floor((Math.random() * 10) + 1);   
+    
+    function restart(){
+        $('#answer').hide();
+        $('#guess').val("");
+        $('#showguess').text("Your Guess will appear here.");
+        $('#close').text("I will be your guide!").show();
+     };
+
+    function hotCold(number) {
+
         if ((number == gotcha)) {
-            alert("You got it!!!");
-            break;
-        } 
-        else if ((number == (gotcha + 1)) || (number == (gotcha - 1))) {
-            alert("Super Hot");
-        } 
-        else if ((number == (gotcha + 2)) || (number == (gotcha - 2))) {
-            alert("Hot");         
-        } 
-        else if ((number == (gotcha + 3)) || (number == (gotcha - 3))) {
-            alert("Warmer");         
-        } 
-        else if ((number == (gotcha + 4)) || (number == (gotcha - 4))) {
-            alert("Warm");         
-        } 
-        else if ((number == (gotcha + 5)) || (number == (gotcha - 5))) {
-            alert("Luke Warm");         
-        } 
-        else {
-            alert("Cold");
+            $('#close').text("You got it!!!").show();
+        } else if ((number == (gotcha + 1)) || (number == (gotcha - 1))) {
+            $('#close').text("Super Hot!!").show();
+        } else if ((number == (gotcha + 2)) || (number == (gotcha - 2))) {
+            $('#close').text("Hot!").show();
+        } else if ((number == (gotcha + 3)) || (number == (gotcha - 3))) {
+            $('#close').text("Warmer").show();
+        } else if ((number == (gotcha + 4)) || (number == (gotcha - 4))) {
+            $('#close').text("Warm").show();
+        } else if ((number == (gotcha + 5)) || (number == (gotcha - 5))) {
+            $('#close').text("Luke Warm").show();
+        } else {
+            $('#close').text("Dead Cold").show();
         }
-    }
-}
+    };
+
+    $('#guesschk').click(function () {
+        $('#showguess').text("You think the number is " + $('#guess').val());
+        hotCold($('#guess').val());
+    });
+
+    $('#giveup').click(function () {
+        $('#answer').text("The answer is " + gotcha).show();
+    });
+    
+    $('#restart').click(function () {
+        restart();
+    })
+
+});
